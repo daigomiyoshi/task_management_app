1a9ae8448ee5d19e2620435e3aaaeab818bedcde
DaigoMiyoshi <daigomiyoshi@DaigoMiyoshi-no-MacBook-Pro.local>
2021-05-18 17:52:34 +0900

    create signup, login, and logout features

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root 'projects#index' 
+  root 'projects#index'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
 end
+ 

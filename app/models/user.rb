fa168268e6026eec97607a9063b1ab2db933840f
DaigoMiyoshi <daigomiyoshi@DaigoMiyoshi-no-MacBook-Pro.local>
2021-05-18 18:49:09 +0900

    change full name in header-nav dynamically

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  def full_name
+    "#{last_name} #{first_name}"
+  end
 end

1470d84b607da30238a5d36e7799f1e25cf50fd3
DaigoMiyoshi <daigomiyoshi@DaigoMiyoshi-no-MacBook-Pro.local>
2021-05-19 10:47:50 +0900

    add. edit user_account features

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -1,5 +1,33 @@
 class UserAccountsController < ApplicationController
+  before_action :set_user_account, only: %i[edit update]
+
   def show; end
 
   def edit; end
+
+  def update
+    if @user_account.update(user_account_params)
+      redirect_to user_account_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.failure')
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user_account
+    @user_account = UserAccount.find(current_user.id)
+  end
+
+  def user_account_params
+    params.require(:user_account).permit(
+      :bank_name, 
+      :bank_branch_name, 
+      :bank_branch_code, 
+      :bank_account_type, 
+      :bank_account_code,
+      :bank_account_name
+    )
+  end
 end

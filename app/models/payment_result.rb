24c3bc3bbdd505d427ee8389741492efd020c54b
DaigoMiyoshi <daigomiyoshi@DaigoMiyoshi-no-MacBook-Pro.local>
2021-05-21 19:21:05 +0900

    show payment result

--- app/models/payment_result.rb
+++ app/models/payment_result.rb
@@ -1,3 +1,6 @@
 class PaymentResult < ApplicationRecord
   belongs_to :user
+  belongs_to :project
+
+  scope :filter_by_user, -> (user_id) { where(user_id: user_id) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root 'projects#index'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
-  resource :user_account, only: %i[show edit update new create destroy]
   get 'projects/:project_id/:year/:month', to: 'work_results#show_monthly', as: 'work_result_monthly'
   get 'projects/:project_id/:year/:month/:day/new', to: 'work_results#new', as: 'new_work_result_daily'
   post 'projects/:project_id/:year/:month/:day/new', to: 'work_results#create'
@@ -14,4 +13,6 @@ Rails.application.routes.draw do
   get 'projects/:project_id/:year/:month/:day/edit', to: 'work_results#edit', as: 'edit_work_result_daily'
   patch 'projects/:project_id/:year/:month/:day/edit', to: 'work_results#update'
   delete 'projects/:project_id/:year/:month/:day', to: 'work_results#destroy', as: 'delete_work_result_daily'
+  resources :payment_results, only: %i[index]
+  resource :user_account, only: %i[show edit update]
 end
